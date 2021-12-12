8323671aa778ae934a9973fe90652faf8f25b859
Anna <anya_shilnikova@mail.ru>
2021-12-12 13:53:22 +0300

    Добавлен вывод цветов на странице админа

--- src/main/java/com/example/goodplants/AllPlantsServlet.java
+++ src/main/java/com/example/goodplants/AllPlantsServlet.java
@@ -9,11 +9,6 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "allPlantsServlet", value = "/allplants-servlet")
 public class AllPlantsServlet extends HttpServlet {
 
-
-    public void init() {
-
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
             /*BufferedReader reader = new BufferedReader(new FileReader("./files/properties.txt"));
@@ -29,12 +24,20 @@ public class AllPlantsServlet extends HttpServlet {
             final String DB_URL = "jdbc:mysql://localhost/goodplant";
             Connection conn = DriverManager.getConnection(DB_URL, user, pass);
             Statement stmt = conn.createStatement();
-            String sql = "SELECT name, family, img FROM plant";
+            String sql = "select plant.id, name, family, plant.desc, img, water, sun, care, toxicity " +
+                    "from plant, water, sun, care, toxicity " +
+                    "where water_id = water.id and sun_id = sun.id and care_id = care.id and toxicity_id = toxicity.id;";
             ResultSet rs = stmt.executeQuery(sql);
             StringBuilder allPlants = new StringBuilder();
             while (rs.next()) {
-                allPlants.append(rs.getString("name")).append(",").append(rs.getString("family"))
-                        .append(",").append(rs.getString("img")).append("|");
+                allPlants.append(rs.getString("id")).append(";").append(rs.getString("name"))
+                        .append(";").append(rs.getString("family"))
+                        .append(";").append(rs.getString("desc"))
+                        .append(";").append(rs.getString("img"))
+                        .append(";").append(rs.getString("water"))
+                        .append(";").append(rs.getString("sun"))
+                        .append(";").append(rs.getString("care"))
+                        .append(";").append(rs.getString("toxicity")).append("|");
             }
             rs.close();
             stmt.close();
@@ -47,8 +50,5 @@ public class AllPlantsServlet extends HttpServlet {
         } catch (IOException | ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public void destroy() {
     }
 }
